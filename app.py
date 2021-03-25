c255eccce76bf538f442689700cfa4767aff0289
Craig Ryan <craig@craigryan.org>
2021-03-25 23:05:03 +0000

    Add Pagination with help from Guido Mentor

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import math
+import pymongo
 from flask import (
     Flask, flash, render_template,
     request, redirect, session, url_for)
@@ -7,6 +9,20 @@ from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
+
+# -----Pagination and sorting params variables ----- #
+PAGE_SIZE = 6
+KEY_PAGE_SIZE = 'page_size'
+KEY_PAGE_NUMBER = 'page_number'
+KEY_TOTAL = 'total'
+KEY_PAGE_COUNT = 'page_count'
+KEY_ENTITIES = 'items'
+KEY_NEXT = 'next_uri'
+KEY_PREV = 'prev_uri'
+KEY_SEARCH_TERM = 'search_term'
+KEY_ORDER_BY = 'order_by'
+KEY_ORDER = 'order'
+
 
 app = Flask(__name__)
 
@@ -19,10 +35,19 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
+# change the name
 @app.route("/home")
 def home():
-    recipes = list(mongo.db.recipes.find())
-    return render_template("index.html", recipes=recipes)
+    # page = request.args.get('page', 1, type=int)
+    if request.method == 'GET':
+        params = request.args.to_dict()
+    else:
+        params = request.form.to_dict()
+    # print(params)
+    paginated_recipes = get_paginated_items(mongo.db.recipes, **params)
+    # recipes = list(mongo.db.recipes.find())
+    # recipes = recipes.query.paginate(page=page, per_page=6)
+    return render_template("index.html", recipes=paginated_recipes)
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -102,19 +127,18 @@ def add_recipe():
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
 
-    if "recipe_image" in request.files:
-        recipe_image = request.files["recipe_image"]
-        if recipe_image != "":
-            mongo.save_file(recipe_image.filename, recipe_image)
-    
     if request.method == "POST":
+        if "recipe_image" in request.files:
+            recipe_image = request.files["recipe_image"]
+            mongo.save_file(recipe_image.filename, recipe_image)
+
         recipe = {
             "recipe_name": request.form.get("recipe_name"),
             "recipe_description": request.form.get("recipe_description"),
             "recipe_time": request.form.get("recipe_time"),
             "recipe_ingredients": request.form.get("recipe_ingredients"),
             "recipe_method": request.form.get("recipe_method"),
-            "recipe_image": request.form.get("recipe_image"),
+            "recipe_image": recipe_image.filename,
             "created_by": session["user"]
         }
         mongo.db.recipes.insert_one(recipe)
@@ -124,15 +148,15 @@ def add_recipe():
     return render_template("add_recipe.html")
 
 
-@app.route("/uploaded_image/<filename>")
-def uploaded_image(filename):
-    return mongo.send_file(filename)
-
-
 @app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
 def edit_recipe(recipe_id):
     username = mongo.db.users.find_one(
             {"username": session["user"]})["username"]
+
+    if "recipe_image" in request.files:
+        recipe_image = request.files["recipe_image"]
+        mongo.save_file(recipe_image.filename, recipe_image)
+
     if request.method == "POST":
 
         submit = {
@@ -152,6 +176,11 @@ def edit_recipe(recipe_id):
     return render_template("edit_recipe.html", recipe=recipe)
 
 
+@app.route("/uploaded_image/<filename>")
+def uploaded_image(filename):
+    return mongo.send_file(filename)
+
+
 @app.route("/delete_recipe/<recipe_id>")
 def delete_recipe(recipe_id):
       username = mongo.db.users.find_one(
@@ -168,6 +197,69 @@ def get_recipe(recipe_id):
     # import pdb;pdb.set_trace()
     return render_template("get_recipe.html",
     recipe=recipe)
+
+
+def get_paginated_items(entity, query={}, **params):  # function
+    page_size = int(params.get(KEY_PAGE_SIZE, PAGE_SIZE))
+    page_number = int(params.get(KEY_PAGE_NUMBER, 1))
+    order_by = params.get(KEY_ORDER_BY, '_id')
+    order = params.get(KEY_ORDER, 'asc')
+    order = pymongo.ASCENDING if order == 'asc' else pymongo.DESCENDING
+
+    # If statement to avoid any pagination issues
+    if page_number < 1:
+        page_number = 1
+    offset = (page_number - 1) * page_size
+    items = []
+
+    # Updated section allow user to paginate a filtered/sorted "query"
+    search_term = params.get(KEY_SEARCH_TERM, '')
+    if bool(query):
+        items = entity.find(query).sort(order_by, order).skip(
+            offset).limit(page_size)
+    else:
+        if search_term != '':
+            entity.create_index([("$**", 'text')])
+            result = entity.find({'$text': {'$search': search_term}})
+            items = result.sort(order_by, order).skip(offset).limit(page_size)
+        else:
+            items = entity.find().sort(
+                order_by, order
+            ).skip(offset).limit(page_size)
+
+    total_items = items.count()
+
+    if page_size > total_items:
+        page_size = total_items
+    if page_number < 1:
+        page_number = 1
+    if page_size:
+        page_count = math.ceil(total_items / page_size)
+    else:
+        page_count = 0
+    if page_number > page_count:
+        page_number = page_count
+    next_uri = {
+        KEY_PAGE_SIZE: page_size,
+        KEY_PAGE_NUMBER: page_number + 1
+    } if page_number < page_count else None
+    prev_uri = {
+        KEY_PAGE_SIZE: page_size,
+        KEY_PAGE_NUMBER: page_number - 1
+    } if page_number > 1 else None
+
+    return {
+        KEY_TOTAL: total_items,
+        KEY_PAGE_SIZE: page_size,
+        KEY_PAGE_COUNT: page_count,
+        KEY_PAGE_NUMBER: page_number,
+        KEY_NEXT: next_uri,
+        KEY_PREV: prev_uri,
+        KEY_SEARCH_TERM: search_term,
+        KEY_ORDER_BY: order_by,
+        KEY_ORDER: order,
+        KEY_ENTITIES: items
+    }
 
 
 if __name__ == "__main__":
