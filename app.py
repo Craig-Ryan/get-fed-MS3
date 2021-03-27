6fe99a3178aa5d4431fb30ed0bee938d7528ec2d
Craig Ryan <craig@craigryan.org>
2021-03-27 14:09:25 +0000

    Add search functionality

--- app.py
+++ app.py
@@ -38,16 +38,15 @@ mongo = PyMongo(app)
 # change the name
 @app.route("/home")
 def home():
-    # page = request.args.get('page', 1, type=int)
-    if request.method == 'GET':
-        params = request.args.to_dict()
-    else:
-        params = request.form.to_dict()
-    # print(params)
-    paginated_recipes = get_paginated_items(mongo.db.recipes, **params)
-    # recipes = list(mongo.db.recipes.find())
-    # recipes = recipes.query.paginate(page=page, per_page=6)
-    return render_template("index.html", recipes=paginated_recipes)
+    recipes = list(mongo.db.recipes.find())
+    return render_template("index.html", recipes=recipes)
+
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    recipes = list(mongo.db.recipes.find({"$text": {"$search": query}}))
+    return render_template("index.html", recipes=recipes)
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -182,69 +181,6 @@ def get_recipe(recipe_id):
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template("get_recipe.html",
     recipe=recipe)
-
-
-def get_paginated_items(entity, query={}, **params):  # function
-    page_size = int(params.get(KEY_PAGE_SIZE, PAGE_SIZE))
-    page_number = int(params.get(KEY_PAGE_NUMBER, 1))
-    order_by = params.get(KEY_ORDER_BY, '_id')
-    order = params.get(KEY_ORDER, 'asc')
-    order = pymongo.ASCENDING if order == 'asc' else pymongo.DESCENDING
-
-    # If statement to avoid any pagination issues
-    if page_number < 1:
-        page_number = 1
-    offset = (page_number - 1) * page_size
-    items = []
-
-    # Updated section allow user to paginate a filtered/sorted "query"
-    search_term = params.get(KEY_SEARCH_TERM, '')
-    if bool(query):
-        items = entity.find(query).sort(order_by, order).skip(
-            offset).limit(page_size)
-    else:
-        if search_term != '':
-            entity.create_index([("$**", 'text')])
-            result = entity.find({'$text': {'$search': search_term}})
-            items = result.sort(order_by, order).skip(offset).limit(page_size)
-        else:
-            items = entity.find().sort(
-                order_by, order
-            ).skip(offset).limit(page_size)
-
-    total_items = items.count()
-
-    if page_size > total_items:
-        page_size = total_items
-    if page_number < 1:
-        page_number = 1
-    if page_size:
-        page_count = math.ceil(total_items / page_size)
-    else:
-        page_count = 0
-    if page_number > page_count:
-        page_number = page_count
-    next_uri = {
-        KEY_PAGE_SIZE: page_size,
-        KEY_PAGE_NUMBER: page_number + 1
-    } if page_number < page_count else None
-    prev_uri = {
-        KEY_PAGE_SIZE: page_size,
-        KEY_PAGE_NUMBER: page_number - 1
-    } if page_number > 1 else None
-
-    return {
-        KEY_TOTAL: total_items,
-        KEY_PAGE_SIZE: page_size,
-        KEY_PAGE_COUNT: page_count,
-        KEY_PAGE_NUMBER: page_number,
-        KEY_NEXT: next_uri,
-        KEY_PREV: prev_uri,
-        KEY_SEARCH_TERM: search_term,
-        KEY_ORDER_BY: order_by,
-        KEY_ORDER: order,
-        KEY_ENTITIES: items
-    }
 
 
 if __name__ == "__main__":
